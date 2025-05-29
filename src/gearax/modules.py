117ab7d51757d0cdb3f900e5d5de5065e699ad1d
yuanz <yuanz271@gmail.com>
2025-05-29 10:21:42 -0400

    tidy

--- src/gearax/modules.py
+++ src/gearax/modules.py
@@ -1,12 +1,27 @@
-from jax import numpy as jnp
+from dataclasses import InitVar
+from pathlib import Path
+from zipfile import ZipFile
+
 import equinox as eqx
+from omegaconf import DictConfig, OmegaConf
 
 
-class ConcatInput(eqx.Module):
-    """Merge"""
+class ConfModule(eqx.Module):
+    conf: DictConfig = eqx.field(static=True)
+    key: InitVar
 
-    network: eqx.Module
-    axis: int = eqx.field(default=0, static=True)
 
-    def __call__(self, *args, **kwargs):
-        return self.network(jnp.concatenate(args, axis=self.axis), **kwargs)
+def save_model(path: str | Path, model: ConfModule):
+    with ZipFile(path, "w") as archive:
+        archive.writestr("conf", OmegaConf.to_yaml(model.conf))
+        with archive.open("pytree", "w") as f:
+            eqx.tree_serialise_leaves(f, model)
+
+
+def load_model(path: str | Path, klass: type) -> ConfModule:
+    with ZipFile(path) as archive:
+        conf = OmegaConf.create(archive.read("conf").decode())
+        model: ConfModule = eqx.filter_eval_shape(klass, OmegaConf.create(conf), None)
+        with archive.open("pytree", "r") as f:
+            model = eqx.tree_deserialise_leaves(f, model)
+    return model
