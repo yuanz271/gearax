fcf7c8f696c0ca7bd05125138f114e44dc6f807e
yuanz <yuanz271@gmail.com>
2025-06-27 11:36:22 -0400

    add annotation for type checker

--- src/gearax/modules.py
+++ src/gearax/modules.py
@@ -1,4 +1,5 @@
 from dataclasses import InitVar
+from typing import Any
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -8,14 +9,14 @@ from omegaconf import DictConfig, OmegaConf
 
 class ConfModule(eqx.Module):
     conf: DictConfig = eqx.field(static=True)
-    key: InitVar
+    key: InitVar[Any]
 
 
 def save_model(path: str | Path, model: ConfModule):
     with ZipFile(path, "w") as archive:
         archive.writestr("conf", OmegaConf.to_yaml(model.conf))
         with archive.open("pytree", "w") as f:
-            eqx.tree_serialise_leaves(f, model)
+            eqx.tree_serialise_leaves(f, model)  # type: ignore
 
 
 def load_model(path: str | Path, klass: type) -> ConfModule:
@@ -23,5 +24,5 @@ def load_model(path: str | Path, klass: type) -> ConfModule:
         conf = OmegaConf.create(archive.read("conf").decode())
         model: ConfModule = eqx.filter_eval_shape(klass, OmegaConf.create(conf), None)
         with archive.open("pytree", "r") as f:
-            model = eqx.tree_deserialise_leaves(f, model)
+            model = eqx.tree_deserialise_leaves(f, model)  # type: ignore
     return model
